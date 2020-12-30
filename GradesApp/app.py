e2cb948efacb0268407a4c9edfdc64bb218ca457
CoolGuy5583 <47202751+CoolGuy5583@users.noreply.github.com>
2020-12-29 18:49:34 -0600

    Add files via upload

--- GradesApp/app.py
+++ GradesApp/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, url_for
+import pandas
 
 app = Flask(__name__)
-
-true, false = True, False
+data = pandas.read_csv(url_for('static', filename="data.csv"))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/calculator')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/gpa')
 def calculator():
     return render_template('calculator.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
